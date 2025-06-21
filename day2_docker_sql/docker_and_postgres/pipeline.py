1f239f2d2bdbb967dc3a3fb56a158fbec0d972fc
I Komang Danda P <dandakomang09@gmail.com>
2025-06-21 17:59:01 +0700

    test other case

--- day2_docker_sql/docker_and_postgres/pipeline.py
+++ day2_docker_sql/docker_and_postgres/pipeline.py
@@ -2,8 +2,17 @@ import sys
 
 import pandas as pd
 
-print(sys.argv)
+# print(sys.argv)
 
-day = sys.argv[1]
+# day = sys.argv[1]
 
-print(f'job finished successfully = {day}')
+# print(f'job finished successfully = {day}')
+
+names = ['Ani', 'Budi', 'Cici']
+ages = []
+for name in names:
+    age_input = input(f"masukkan umur untuk {name}: ")
+    ages.append(int(age_input))
+data = {'Name': names, 'Age': ages}
+df = pd.DataFrame(data)
+print(df)
